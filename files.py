94f2a2f0d88f27caa33ee710e01583003a29e51c
me <me@robertcurtiss.com>
2023-06-11 21:56:25 -0400

    Removed bet data from files.py

--- files.py
+++ files.py
@@ -5,6 +5,7 @@ from collections import defaultdict, namedtuple
 from operator import itemgetter
 from pathlib import Path
 from time import strptime
+from bet_data import BetData
 
 DATE_FORMAT = "%m-%d-%Y"
 file_path = Path.home() / "beerme" / "data"
@@ -27,91 +28,10 @@ class ProcessDataFiles:
     def __init__(self):
         self.race_schedule_results = []
         self.bets_team = []
-        self.individual_bets = defaultdict()
-        # self.individual_bets.setdefault("missing_key")
-        self.individual_bets["02-19-2023"] = {
-            "Greg": "Kyle Larson",
-            "Bob": "Ryan Blaney",
-            "badge_color": "bg-warning text-dark",
-        }
-        self.individual_bets["02-26-2023"] = {
-            "Greg": "Ryan Blaney",
-            "Bob": "Joey Logano",
-            "badge_color": "bg-warning text-dark",
-        }
-        self.individual_bets["03-05-2023"] = {
-            "Greg": "Kyle Larson",
-            "Bob": "Kyle Busch",
-            "badge_color": "bg-warning text-dark",
-        }
-        self.individual_bets["03-12-2023"] = {
-            "Greg": "Ryan Blaney",
-            "Bob": "William Byron",
-            "badge_color": "bg-warning text-dark",
-        }
-        self.individual_bets["03-19-2023"] = {
-            "Greg": "Kyle Busch",
-            "Bob": "Joey Logano",
-            "badge_color": "bg-warning text-dark",
-        }
-        self.individual_bets["03-26-2023"] = {
-            "Greg": "Tyler Reddick",
-            "Bob": "Ross Chastain",
-            "badge_color": "bg-warning text-dark",
-        }
-        self.individual_bets["04-02-2023"] = {
-            "Greg": "Kyle Larson",
-            "Bob": "Kevin Harvick",
-            "badge_color": "bg-warning text-dark",
-        }
-        self.individual_bets["04-09-2023"] = {
-            "Greg": "Kyle Larson",
-            "Bob": "Tyler Reddick",
-            "badge_color": "bg-warning text-dark",
-        }
-        self.individual_bets["04-16-2023"] = {
-            "Greg": "William Byron",
-            "Bob": "Tyler Reddick",
-            "badge_color": "bg-warning text-dark",
-        }
-        # start of can't pick a driver twice
-        self.individual_bets["04-23-2023"] = {
-            "Greg": "Ryan Blaney",
-            "Bob": "Aric Almirola",
-            "badge_color": "bg-success text-light",
-        }
-        self.individual_bets["04-30-2023"] = {
-            "Greg": "Kyle Larson",
-            "Bob": "William Byron",
-            "badge_color": "bg-success text-light",
-        }
-        self.individual_bets["05-07-2023"] = {
-            "Greg": "William Byron",
-            "Bob": "Martin Truex Jr.",
-            "badge_color": "bg-success text-light",
-        }
-        self.individual_bets["05-14-2023"] = {
-            "Greg": "Denny Hamlin",
-            "Bob": "Kyle Larson",
-            "badge_color": "bg-success text-light",
-        }
-        self.individual_bets["05-28-2023"] = {
-            "Greg": "Chase Elliott",
-            "Bob": "Kyle Busch",
-            "badge_color": "bg-success text-light",
-        }
-        self.individual_bets["06-04-2023"] = {
-            "Greg": "Kevin Harvick",
-            "Bob": "Joey Logano",
-            "badge_color": "bg-success text-light",
-        }
-        self.individual_bets["06-11-2023"] = {
-            "Greg": "Tyler Reddick",
-            "Bob": "AJ Allmendinger",
-            "badge_color": "bg-success text-light",
-        }
         # print(f"{self.individual_bets}")
         # print(self.individual_bets)
+        data = BetData()
+        self.individual_bets = data.get_bets()
 
     # @property
     def bets(self):
