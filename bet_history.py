9710c9fdb598c143b50ed0da17e8026c9ab95c3c
me <me@robertcurtiss.com>
2023-08-08 08:26:36 -0400

    bet_history

--- bet_history.py
+++ bet_history.py
@@ -7,18 +7,30 @@ data = BetData()
 all_bets = data.get_bets
 
 
-def look_driver_up(driver_key_name):
-    return True
+def look_driver_up(race_date,player_name,driver_name):
+    if player_name == 'Bob':
+        player_name = 'Greg'
+    else:
+        player_name = 'Bob'
+    print(f'\nplayer_name={player_name}, driver_name={driver_name}')
+    # return all_bets[race_date][player_name] == driver_name
+    for x in all_bets[race_date]:
+        if x == "badge_color":
+            continue
+        print(f'x={x}')
+        if all_bets[race_date][x] == driver_name:
+            return True
+    return False
+    return [x for x in all_bets[race_date][player_name] if x == driver_name]
 
 
 for race_date in all_bets:
     if race_date >= "04-23-2023":
         print(f"{race_date}", end=" - ")
-        for key_name in all_bets[race_date]:
-            if key_name != "badge_color":
-                print(f"{all_bets[race_date]['Bob']:6s}", end=" ")
+        for player_name in all_bets[race_date]:
+            if player_name != "badge_color":
+                print(f"{player_name:6s}", end=" = ")
                 print(
-                    f"{look_driver_up(key_name)} {all_bets[race_date][key_name]:16s}",
-                    end=" - ",
+                    f"{look_driver_up(race_date,player_name, all_bets[race_date][player_name])} {all_bets[race_date][player_name]:16s}",
                 )
     print()
