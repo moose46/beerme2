5efe4c534507c1db4c6574e22fd7119e109c777c
me <me@robertcurtiss.com>
2023-08-14 08:22:23 -0400

    Bet_History update

--- bet_history.py
+++ bet_history.py
@@ -7,30 +7,34 @@ data = BetData()
 all_bets = data.get_bets
 
 
-def look_driver_up(race_date,player_name,driver_name):
-    if player_name == 'Bob':
-        player_name = 'Greg'
-    else:
-        player_name = 'Bob'
-    print(f'\nplayer_name={player_name}, driver_name={driver_name}')
-    # return all_bets[race_date][player_name] == driver_name
-    for x in all_bets[race_date]:
-        if x == "badge_color":
-            continue
-        print(f'x={x}')
-        if all_bets[race_date][x] == driver_name:
-            return True
-    return False
-    return [x for x in all_bets[race_date][player_name] if x == driver_name]
+def look_driver_up(player_name: str, driver_name: str) -> bool:
+    return any(
+        race_date >= "04-23-2023" and all_bets[race_date][player_name] == driver_name
+        for race_date in all_bets
+    )
 
 
 for race_date in all_bets:
-    if race_date >= "04-23-2023":
-        print(f"{race_date}", end=" - ")
-        for player_name in all_bets[race_date]:
-            if player_name != "badge_color":
-                print(f"{player_name:6s}", end=" = ")
-                print(
-                    f"{look_driver_up(race_date,player_name, all_bets[race_date][player_name])} {all_bets[race_date][player_name]:16s}",
-                )
+    if race_date < "04-23-2023":
+        continue
+    for player_name in all_bets[race_date]:
+        if player_name != "badge_color":
+            is_avaiable = look_driver_up(player_name, all_bets[race_date][player_name])
+            print(f"{player_name:6s} {is_avaiable}", end=" = ")
+            print(
+                f"{'Available' if is_avaiable == False else 'Picked'} {all_bets[race_date][player_name]:16s}",
+            )
     print()
+
+
+print(f'{look_driver_up("Bob", "Ryan Blaney")}')
+print(f'{look_driver_up("Bob", "Tyler Reddick")}')
+print(f'{look_driver_up("Bob", "Denny Hamlin")}')
+
+
+print(
+    any(
+        race_date >= "04-23-2023" and all_bets[race_date]["Bob"] == "Tyler Redddick"
+        for race_date in all_bets
+    )
+)
